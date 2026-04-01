41d3d0a17eb9bd5840a7f9870f4fee2ef97c68d2
InkedCat <146424693+InkedCat@users.noreply.github.com>
2026-04-01 16:58:25 +0200

    chore: add linting and formating

---
+++ eslint.config.ts
@@ -0,0 +1,26 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  eslintPluginPrettier,
+  {
+    ignores: ["dist/", "node_modules/", "src/generated/"],
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+);
